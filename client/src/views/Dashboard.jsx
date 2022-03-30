dcd1a93e51d90ea612f8ee5bc69785b784b069ab
John Cuevas <jpmcuevas20@gmail.com>
2022-03-30 13:44:44 -0700

    Carousel styling

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -8,9 +8,9 @@ const Dashboard = () => {
     const carousel = 
         [
             {id: 1, source: 'https://cdn.shopify.com/s/files/1/2541/4208/products/wholesale-succulent-plants-for-sale-by-succy-crafts-314732.jpg?v=1623784454'},
-            {id: 2, source: 'https://cdn11.bigcommerce.com/s-oqm1pc/images/stencil/631x631/products/2918/9362/set9_cactus__79311.1629325859.jpg?c=2'},
+            {id: 2, source: 'https://www.gardeningknowhow.com/wp-content/uploads/2020/12/cacti-and-succulents-690x518.jpg'},
             {id: 3, source: 'https://cdn.cdnparenting.com/articles/2019/01/12114103/519235318-H.jpg'},
-            {id: 4, source: 'https://cdn.shopify.com/s/files/1/0662/5489/files/variegated-monstera-albo-variegata-variegated-indoor-plants-pistils-nursery-6.jpg?v=1572628630'},
+            {id: 4, source: 'https://cityfloralgreenhouse.com/wp-content/uploads/2021/01/houseplant-trend-header.jpg'},
             {id: 5, source: 'https://cdn.shopify.com/s/files/1/2528/3612/products/Easy-Care-Bundle-animation_800px_800x800.gif?v=1621115461'}
         ]
     
