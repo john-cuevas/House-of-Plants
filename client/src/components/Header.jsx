f18c4aff1bbe9b9088effe8ea890e1b799070d96
John Cuevas <jpmcuevas20@gmail.com>
2022-04-01 11:05:30 -0700

    User Name

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Link, useHistory} from 'react-router-dom'
 import axios from 'axios'
 
 
 const Header = () => {
     let history = useHistory()
+    const [user, setUser] = useState("")
 
     const handleLogOut = ()=> {
         axios.get(`http://localhost:8000/api/logout`,{withCredentials: true})
@@ -12,15 +13,39 @@ const Header = () => {
             .catch()
     }
 
+    useEffect(() =>{
+        axios.get(`http://localhost:8000/api/user`,{withCredentials: true})
+            .then(res => setUser(res.data))
+            // .then(setRefresh(!refresh))
+            .catch(err => console.log(err))
+    }, )
+
+
     return (
         <div className='NavbarContainer'>
             <div className='navbar1'>
                 <h1><Link to={"/"}>House of Plants</Link></h1>
                 <div className='navbarlink'>
+                    {
+                        user?
+                            <div >
+                                <h4>Welcome {user.firstName}!</h4>
+                                <Link to="/favorites"><button>Favorites</button></Link>
+                                <button onClick={handleLogOut}>Sign Out</button>
+                            </div>
+                        :
+                            <div >
+                                <Link to="/login"> <button>Sign In</button></Link>
+                            </div>
+
+                    }
+                </div>
+
+                {/* <div className='navbarlink'>
                     <Link to="/favorites"><button>Favorites</button></Link>
                     <Link to="/login"> <button>Sign In</button></Link>
-                    <Link><button onClick={handleLogOut}>Sign Out</button></Link>
-                </div>
+                    <button onClick={handleLogOut}>Sign Out</button>
+                </div> */}
                 
             </div>
             <div className='links navbar'>
